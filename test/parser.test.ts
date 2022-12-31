51b1292aba71c3a7f555bdbe231075dafb550ee9
Billy Onjea <vasili.onjea@gmail.com>
2022-12-31 07:23:53 +0200

    added tests

--- test/parser.test.ts
+++ test/parser.test.ts
@@ -16,16 +16,21 @@ test('it should parse tokens to a query object', () => {
 
   expect(query.parts[0].term).toBe('frontend');
   expect(query.parts[0].type).toBe(QueryPartType.Required);
+  expect(query.parts[0].isPhrase).toBe(false);
 
   expect(query.parts[1].term).toBe('engineer');
   expect(query.parts[1].type).toBe(QueryPartType.Simple);
+  expect(query.parts[1].isPhrase).toBe(false);
 
   expect(query.parts[2].term).toBe('backend');
   expect(query.parts[2].type).toBe(QueryPartType.Negated);
+  expect(query.parts[2].isPhrase).toBe(false);
 
   expect(query.parts[3].term).toBe('ux engineer');
+  expect(query.parts[3].isPhrase).toBe(true);
   expect(query.parts[3].type).toBe(QueryPartType.Simple);
 
   expect(query.parts[4].term).toBe('full stack');
+  expect(query.parts[4].isPhrase).toBe(true);
   expect(query.parts[4].type).toBe(QueryPartType.Negated);
 });

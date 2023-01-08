4424eb149e584d24f372ce0fac06b1f46840f539
Billy Onjea <vasili.onjea@gmail.com>
2023-01-08 17:19:59 +0200

    refactor index stats script

---
+++ scripts/index-stats.js
@@ -0,0 +1,31 @@
+import { readFile } from 'node:fs/promises';
+import path from 'node:path';
+import url from 'node:url';
+import sizeof from 'object-sizeof';
+import prettyBytes from 'pretty-bytes';
+
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+
+function logStats(label = '', data = {}) {
+  const stats = {
+    size: prettyBytes(Buffer.byteLength(data)),
+    memory: prettyBytes(sizeof(data))
+  };
+
+  console.log('\n', label);
+  console.table([stats]);
+}
+
+async function runStats() {
+  const files = [
+    { label: 'Index before:', name: 'index-before.json' },
+    { label: 'Index after:', name: 'index-after.json' }
+  ];
+
+  for (const file of files) {
+    const data = await readFile(path.join(__dirname, file.name), { encoding: 'utf-8' });
+    logStats(file.label, data);
+  }
+}
+
+runStats();

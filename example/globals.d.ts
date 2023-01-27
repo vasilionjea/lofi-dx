4ffb9335516418a3b1713fd47e1f1d304e27d4d5
Billy Onjea <vasili.onjea@gmail.com>
2023-01-28 01:15:28 +0200

    adds ambient global modules for image Webpack modules so they can be imported in a ts file

--- example/globals.d.ts
+++ example/globals.d.ts
@@ -1,1 +1,7 @@
 // Add global vars here
+
+// https://www.typescriptlang.org/docs/handbook/modules.html#wildcard-module-declarations
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.svg';

4fb30db31965ad6d0a633b958618803d4c875d28
Billy Onjea <vasili.onjea@gmail.com>
2023-01-30 02:29:14 +0200

    refactor

--- example/components/input.ts
+++ example/components/input.ts
@@ -1,18 +1,20 @@
 import CoreComponent from './core';
 
+const TEXT_PLACEHOLDER = 'Search for something...';
+
 /**
  * Search Input
  */
 export default class SearchInput extends CoreComponent {
-  inputElement!: HTMLInputElement;
-  btnClearElement!: HTMLButtonElement;
+  input!: HTMLInputElement;
+  clearButton!: HTMLButtonElement;
 
   get classNames() {
     return ['search-input'];
   }
 
   private onInputChange() {
-    if (this.inputElement!.value) {
+    if (this.input!.value) {
       this.dispatchInput();
     } else {
       this.dispatchClear();
@@ -22,7 +24,7 @@ export default class SearchInput extends CoreComponent {
   private dispatchInput() {
     const event = new CustomEvent('input:value', {
       bubbles: true,
-      detail: { value: this.inputElement!.value },
+      detail: { value: this.input!.value },
     });
 
     this.dispatchEvent(event);
@@ -34,46 +36,43 @@ export default class SearchInput extends CoreComponent {
   }
 
   setValue(value: string) {
-    if (!this.inputElement) return;
+    if (!this.input) return;
 
-    this.inputElement.value = value;
+    this.input.value = value;
     this.onInputChange();
   }
 
+  reset() {
+    this.setValue('');
+    this.dispatchClear();
+    this.input?.focus();
+  }
+
   private clearIconTemplate() {
-    return `<svg aria-hidden="true" viewBox="0 0 24 24" focusable="false" style="pointer-events: none; display: block; width: inherit; height: inherit" class=""><g><path d="M12 2C6.47 2 2 6.47 2 12s4.47 10 10 10 10-4.47 10-10S17.53 2 12 2zm5 13.59L15.59 17 12 13.41 8.41 17 7 15.59 10.59 12 7 8.41 8.41 7 12 10.59 15.59 7 17 8.41 13.41 12 17 15.59z"></path></g></svg>`;
+    let str = `<svg aria-hidden="true" viewBox="0 0 24 24" focusable="false" style="pointer-events: none; display: block; width: inherit; height: inherit">`;
+    str += `<g><path d="M12 2C6.47 2 2 6.47 2 12s4.47 10 10 10 10-4.47 10-10S17.53 2 12 2zm5 13.59L15.59 17 12 13.41 8.41 17 7 15.59 10.59 12 7 8.41 8.41 7 12 10.59 15.59 7 17 8.41 13.41 12 17 15.59z"></path></g>`;
+    return str + '</svg>';
   }
 
   private template() {
     const labelId = 'prompt';
     const inputId = 'searchInput';
-    const placeholder = 'Search for something...';
 
-    let tpl = `<label id="${labelId}" for="${inputId}" class="visuallyhidden">Search national parks</label>`;
-    tpl += `<input type="search" id="${inputId}" placeholder="${placeholder}" aria-labelledby="${labelId}" autocorrect="off" spellcheck="false" autocapitalize="off">`;
-    tpl += `<button class="btn btn-icon btn-clear" aria-label="Clear search">${this.clearIconTemplate()}</button>`;
+    let str = `<label id="${labelId}" for="${inputId}" class="visuallyhidden">Search national parks</label>`;
+    str += `<input type="search" id="${inputId}" placeholder="${TEXT_PLACEHOLDER}" aria-labelledby="${labelId}" autocorrect="off" spellcheck="false" autocapitalize="off">`;
+    str += `<button class="btn btn-icon btn-clear" aria-label="Clear search">${this.clearIconTemplate()}</button>`;
 
-    return tpl;
+    return str;
   }
 
   render() {
     this.element.insertAdjacentHTML('beforeend', this.template());
-    this.inputElement = this.element.querySelector('input')!;
-    this.btnClearElement = this.element.querySelector('.btn-clear')!;
 
-    if (this.inputElement) {
-      this.inputElement.addEventListener('input', () => this.onInputChange());
-    }
+    this.input = this.element.querySelector('input')!;
+    this.input.addEventListener('input', () => this.onInputChange());
 
-    if (this.btnClearElement) {
-      this.btnClearElement.addEventListener('click', (e) => {
-        e.stopPropagation();
-
-        this.setValue('');
-        this.dispatchClear();
-        this.inputElement?.focus();
-      });
-    }
+    this.clearButton = this.element.querySelector('.btn-clear')!;
+    this.clearButton.addEventListener('click', (e) => this.reset());
 
     return this;
   }

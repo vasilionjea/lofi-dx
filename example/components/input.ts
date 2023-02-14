459480a367ad0e9de74e2bad3ad2df2a617afffa
Billy Onjea <vasili.onjea@gmail.com>
2023-02-14 13:47:10 +0100

    cleanup

--- example/components/input.ts
+++ example/components/input.ts
@@ -48,20 +48,27 @@ export default class SearchInput extends CoreComponent {
   }
 
   private clearIconTemplate() {
-    let str = `<svg aria-hidden="true" viewBox="0 0 24 24" focusable="false" style="pointer-events: none; display: block; width: inherit; height: inherit">`;
-    str += `<g><path d="M12 2C6.47 2 2 6.47 2 12s4.47 10 10 10 10-4.47 10-10S17.53 2 12 2zm5 13.59L15.59 17 12 13.41 8.41 17 7 15.59 10.59 12 7 8.41 8.41 7 12 10.59 15.59 7 17 8.41 13.41 12 17 15.59z"></path></g>`;
-    return str + '</svg>';
+    return `
+      <svg
+        aria-hidden="true"
+        viewBox="0 0 24 24"
+        focusable="false"
+        style="pointer-events:none; display:block; width:inherit; height:inherit">
+        <g>
+          <path d="M12 2C6.47 2 2 6.47 2 12s4.47 10 10 10 10-4.47 10-10S17.53 2 12 2zm5 13.59L15.59 17 12 13.41 8.41 17 7 15.59 10.59 12 7 8.41 8.41 7 12 10.59 15.59 7 17 8.41 13.41 12 17 15.59z"></path>
+        </g>
+      </svg>
+    `;
   }
 
   private template() {
     const labelId = 'prompt';
     const inputId = 'searchInput';
-
-    let str = `<label id="${labelId}" for="${inputId}" class="visuallyhidden">Search national parks</label>`;
-    str += `<input type="search" id="${inputId}" placeholder="${TEXT_PLACEHOLDER}" aria-labelledby="${labelId}" autocorrect="off" spellcheck="false" autocapitalize="off">`;
-    str += `<button class="btn btn-icon btn-clear" aria-label="Clear search">${this.clearIconTemplate()}</button>`;
-
-    return str;
+    return `
+      <label id="${labelId}" for="${inputId}" class="visuallyhidden">Search national parks</label>
+      <input type="search" id="${inputId}" placeholder="${TEXT_PLACEHOLDER}" aria-labelledby="${labelId}" autocorrect="off" spellcheck="false" autocapitalize="off">
+      <button class="btn btn-icon btn-clear" aria-label="Clear search">${this.clearIconTemplate()}</button>
+    `;
   }
 
   render() {
